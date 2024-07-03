3d501eea288cd5e61e9067b8ffd5df85246351c8
vihao <tranvihao40@gmail.com>
2024-07-03 20:32:23 +0700

    refactor(LessonsShowcase): change UI lessons rendered

--- src/components/LessonsShowcase.tsx
+++ src/components/LessonsShowcase.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from "react";
-import { Lesson } from "../../types";
+import { ILesson } from "../../types";
 import toast from "react-hot-toast";
 import { GetAllLessonByCourseId } from "@/lib/actions/lesson.action";
 import Image from "next/image";
 import Link from "next/link";
 import { Skeleton } from "./ui/skeleton";
+import { formatSecond } from "@/lib/utils";
 
 const LessonsShowcase = ({ courseId }: { courseId: string }) => {
-  const [lessons, setLessons] = useState<Lesson[] | []>([]);
+  const [lessons, setLessons] = useState<ILesson[] | []>([]);
   const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -30,35 +31,42 @@ const LessonsShowcase = ({ courseId }: { courseId: string }) => {
     <div className="mt-4">
       <h2 className="text-xl font-bold">Lessons</h2>
       <div className="flex flex-col mt-3 border-2 p-2 rounded-md gap-4">
-        {(isLoading || !lessons) && (
+        {isLoading && (
           <>
-            {Array.from({ length: 4 }).map((_, index) => {
-              <Skeleton key={index} className="w-full h-6" />;
-            })}
+            {Array.from({ length: 4 }).map((_, index) => (
+              <Skeleton key={index} className="w-full h-12 bg-gray-400" />
+            ))}
           </>
         )}
-
-        {!isLoading && lessons && (
-          <div className="relative flex flex-row justify-between w-full px-4 py-2 rounded-lg pd-2 hover:bg-slate-200">
-            <Link
-              href={`/course/detail/1/lesson/1`}
-              className="absolute w-full h-full top-0 left-0"
-            ></Link>
-            <div className="w-[95%]">
-              <h2 className="font-bold text-lg line-clamp-1">
-                Web Programming Introduce Programming Introduce
-              </h2>
-              <p className="text-sm text-gray-700">Lesson 1 - 4min</p>
+        {!isLoading && lessons.length === 0 && <h1>No lesson available</h1>}
+        {!isLoading &&
+          lessons.length > 0 &&
+          lessons.map((lesson, index) => (
+            <div
+              key={lesson.id}
+              className="relative flex flex-row justify-between w-full px-4 py-2 rounded-lg pd-2 hover:bg-gray-200"
+            >
+              <Link
+                href={`/course/detail/${courseId}/lesson/${lesson.id}`}
+                className="absolute w-full h-full top-0 left-0"
+              ></Link>
+              <div className="w-[95%]">
+                <h2 className="font-bold text-lg line-clamp-1">
+                  {lesson.title}
+                </h2>
+                <p className="text-sm text-gray-700">
+                  Lesson {index + 1} - {formatSecond(lesson.video.duration)}
+                </p>
+              </div>
+              <Image
+                src={"/chevron-right.svg"}
+                alt="Logo next"
+                width="0"
+                height="0"
+                className="p-[6px] w-[5%]"
+              />
             </div>
-            <Image
-              src={"/chevron-right.svg"}
-              alt="Logo next"
-              width="0"
-              height="0"
-              className="p-[6px] w-[5%]"
-            />
-          </div>
-        )}
+          ))}
       </div>
     </div>
   );
