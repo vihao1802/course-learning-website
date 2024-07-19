4157488b1885233d53bc32b3b1ffcea7bf9fc7f0
vihao <tranvihao40@gmail.com>
2024-07-19 16:22:55 +0700

    feat(page/sign-in): add generateStaticParams

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
+export async function generateStaticParams() {
+  return [{ params: { "sign-in": [] } }];
+}
+
 export default function Page() {
   return (
     <main className="w-full h-full flex justify-center items-start">
